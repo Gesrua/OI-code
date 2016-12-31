54b08d617f2ba266fe43ee14f5c355745ce04432
Coder <coder@CoderdeDevice.local>
2016-12-31 16:21:40 +0800

    P1106

---
+++ complete/P1106.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string s;
+int n;
+int main()
+{
+	getline(cin,s);
+	int cur=0,i;
+	cin>>n;
+	while (cur!=n)
+	{
+		i = 0;
+		for (i=0;i<s.length();i++)
+			if (s[i]>s[i+1])
+			{
+				s.erase(i,1);
+				break;
+			}
+		cur++;
+	}
+	while (s[0]=='0') s.erase(0,1);
+	for (i=0;i<s.length();i++) cout<<s[i];
+	return 0;
+}
+
