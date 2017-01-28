b121722208f52d3a1ebda3740ddf3c880a2140e4
Coder <huaibo2016@gmail.com>
2017-01-28 17:29:10 +0800

    two problem done

---
+++ complete/P1192.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	int i,j,n,k,a[200000];
+	std::cin>>n>>k;
+	std::memset(a,0,sizeof(a));
+	for (i=1;i<=k;i++) a[i] = 1;
+	for (i=1;i<n;i++)
+	 	for (j=1;j<=k;j++)
+			if (a[i] != 0) {
+				a[i+j] += a[i];
+				a[i+j] = a[i+j] % 100003;
+			}
+	std::cout<<a[n];
+    return 0;
+}
