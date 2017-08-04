a3251cde20941452e54e20daf85e1845e8a00b49
huaibo2003 <huaibo2016@gmail.com>
2017-08-04 12:36:57 +0800

    P1425

---
+++ complete/P1425.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+struct time{
+    int h,m;
+};
+int main() {
+    struct time start,end;
+    std::cin >> start.h >> start.m >> end.h >> end.m;
+    if (start.m > end.m){
+        end.h--;
+        end.m += 60;
+    }
+    std::cout << end.h - start.h << ' ' << end.m - start.m;
+    return 0;
+}
