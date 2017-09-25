8beb86c8374b2be5352e54422d4234c6b140a1a7
Dark_Night <huaibo2016@gmail.com>
2017-09-25 12:46:34 +0800

    P1208

---
+++ complete/P1208.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+struct cell{
+  int p, a;
+}a[5001];
+bool cmp(cell a,cell b){
+  return a.p < b.p;
+}
+int main(){
+  using namespace std;
+  int ans = 0, n, m, i;
+  cin >> n >> m;
+  for (i=1;i<=m;i++) cin >> a[i].p >> a[i].a;
+  sort(a+1,a+1+m,cmp);
+  for (i=1;a[i].a < n;i++){
+    n -= a[i].a;
+    ans += a[i].a * a[i].p;
+  }
+  if (n!=0) ans += a[i].p * n;
+  cout << ans;
+  return 0;
+}
