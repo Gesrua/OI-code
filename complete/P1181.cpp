7d5db2de1b15949b7f75d83950ddbcf413a55ef9
Dark_Night <huaibo2016@gmail.com>
2017-09-25 17:48:35 +0800

    P1181

---
+++ complete/P1181.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main(){
+  int n, m, i, t, ans = 0, sum = 0;
+  using namespace std;
+  cin >> n >> m;
+  for (i=0;i<n;i++){
+    cin >> t;
+    if (m <= sum + t){
+      if (m == sum + t){
+        ans++;
+        sum = 0;
+      }
+      else{
+        ans++;
+        sum = t;
+      }
+    }
+    else{
+      if (i == n-1) ans++;
+      else sum += t;
+    }
+  }
+  cout << ans;
+  return 0;
+}
