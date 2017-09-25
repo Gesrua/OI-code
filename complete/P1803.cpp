b974b92d0fcb1ea2bc6867e0b6af93518564e0ad
Dark_Night <huaibo2016@gmail.com>
2017-09-25 12:30:16 +0800

    P1803

---
+++ complete/P1803.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+struct cell{
+  int start, end;
+}a[100001];
+int n;
+bool cmp(cell a,cell b){
+  return a.end < b.end;
+}
+int main(){
+  using namespace std;
+  cin >> n;
+  int i, ans = 1, t;
+  for (i=1;i<=n;i++) cin >> a[i].start >>  a[i].end;
+  sort(a+1,a+1+n,cmp);
+  t = a[1].end;
+  for (i=2;i<=n;i++){
+    if (a[i].start >= t){
+      ans++;
+      t = a[i].end;
+    }
+  }
+  cout << ans;
+  return 0;
+}
