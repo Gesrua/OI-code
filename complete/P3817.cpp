57541ddfb482df1fd845d82e73aab1352afa6f25
Dark_Night <huaibo2016@gmail.com>
2017-10-01 18:35:50 +0800

    P3817

---
+++ complete/P3817.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+int main(){
+  long long n, x, i, ans = 0, t1, t2;
+  std::cin >> n >> x;
+  std::cin >> t1;
+  for (i=0;i<n-1;i++){
+    std::cin >> t2;
+    if (t1 + t2 > x){
+      ans = ans + t1 + t2 - x;
+      t1 = t2 - (t1 + t2 - x);
+      if (t1 < 0) t1 = 0;
+    }
+    else t1 = t2;
+  }
+  std::cout << ans;
+  return 0;
+}
