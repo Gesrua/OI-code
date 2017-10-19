a59d35321cbc33b7fd90f408056905fec718506c
Dark_Night <huaibo2016@gmail.com>
2017-10-20 07:43:54 +0800

    P1030

---
+++ complete/P1030.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+std::string I, P;
+void solve(const int Il,const int Ir,const int Pl, const int Pr){
+  int pos = I.find(P[Pr]);
+  std::cout << P[Pr];
+  if (Il < pos) {
+    solve(Il,pos-1,Pl,pos-Il+Pl-1);
+  }
+  if (pos < Ir){
+    solve(pos+1,Ir,pos-Il+Pl,Pr-1);
+  }
+}
+int main(){
+  using std::cin;
+  cin >> I >> P;
+  solve(0,I.size()-1,0,I.size()-1);
+  return 0;
+}
