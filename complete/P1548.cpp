22b3bcbfc04b247076b68a4e067d1211e824c240
Dark_Night <huaibo2016@gmail.com>
2017-10-20 11:15:28 +0800

    P1548

---
+++ complete/P1548.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+int main(){
+  int n ,m, ans_chang = 0, ans_zheng = 0;
+  std::cin >> n >> m;
+  for (int i=1;i<=n;i++){
+    for (int j=1;j<=m;j++){
+      if (i==j){
+        ans_zheng += (n-i+1) * (m-j+1);
+      }else{
+        ans_chang += (n-i+1) * (m-j+1);
+      }
+    }
+  }
+  std::cout << ans_zheng << ' ' << ans_chang;
+  return 0;
+}
