fa50f3c8d3c2c07192df28c045621dbdbafae2c6
Dark_Night <huaibo2016@gmail.com>
2017-10-22 00:28:21 +0800

    P2142

--- complete/P2142.cpp
+++ complete/P2142.cpp
@@ -7,14 +7,14 @@ int main(){
 	int tot;
 	cin >> s1;
 	cin >> s2;
-	int a[1001];
+	int a[100000];
 	
 	if (s1.size() < s2.size()){
 		cout<<'-';
 		std::swap(s1, s2);
 	}
 	if (s1.size() == s2.size()){
-		for (register int i=0;i<s1.size();++i){
+		for (int i=0;i<s1.size();++i){
 			if (s1[i] < s2[i]){
 				std::swap(s1, s2);
 				cout << '-';
@@ -26,16 +26,16 @@ int main(){
 	//cout << endl << s1 << ' ' << s2;
 	
 	tot = s1.size();
-	for (register int i=1;i<=s1.size();++i)
+	for (int i=1;i<=s1.size();++i)
 		a[i]  = s1[tot-i] - '0';
 	
-	for (register int i=1;i<=s2.size();++i)
+	for (int i=1;i<=s2.size();++i)
 		a[i] -= s2[s2.size()-i] - '0';
 	
-	for (register int i=1;i<=tot;++i){
+	for (int i=1;i<=tot;++i){
 		if (a[i] < 0){
-			a[i+1] -= (-a[i])/10 + 1;
-			a[i] = 10 - std::abs(a[i])%10;
+			--a[i+1];
+			a[i] += 10;
 		}
 	}
 	while ((a[tot] == 0) && (tot > 0)) --tot;
@@ -43,7 +43,7 @@ int main(){
 		cout << '0';
 		return 0;
 	}
-	for (register int i=tot;i>0;--i)
+	for (int i=tot;i>0;--i)
 		cout << a[i];
 	return 0;
 }
