522a38bba40d6d43b4e0e7f0976a74cbd8acaf61
chaigidel <huaibo2016@gmail.com>
2017-10-21 09:41:24 +0800

    P1096

---
+++ complete/P1096.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+int n, mod = 10;
+int main(){
+	using std::cin;
+	using std::cout;
+	using std::endl;
+	cin >> n;
+	int a[200] = {}, tot = 1, i, j;
+	a[1] = 1;
+	for (i = 2; i <= n; ++i){
+		// *2
+		for (j=1;j<=tot;++j)
+			a[j] *= 2;
+		++a[1];
+		for (j=1;j<=tot;++j){
+			a[j+1] = a[j+1] + a[j] / mod;
+			a[j] %= mod;
+		}
+		if (a[tot+1] != 0) ++tot;
+		
+	}
+	
+	for (j=1;j<=tot;++j)
+		a[j] *= 2;
+	for (j=1;j<=tot;++j){
+		a[j+1] = a[j+1] + a[j] / mod;
+		a[j] %= mod;
+	}
+	if (a[tot+1] != 0) ++tot;
+	for (i=tot;i>=1;i--){
+		cout << a[i];
+	}
+	
+	return 0;
+}
