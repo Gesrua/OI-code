3db01b60939491b88809e5f71bb23a35e0a1093a
chaigidel <huaibo2016@gmail.com>
2017-11-08 12:47:47 +0800

    P1115

---
+++ complete/P1115.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using std::cin;
+using std::cout;
+using std::endl;
+//const int N = 200000;
+std::queue<int> q;
+int main(){
+	std::ios::sync_with_stdio(false);
+	int t, n, ans, max = -2100000000, sum = 0;
+	cin >> n;
+	while (n){
+		cin >> t;
+		if (sum < 0){
+			std::queue<int> empty;
+			std::swap(empty, q);
+			sum = 0;
+		}
+		q.push(t);
+		sum += t;
+		if (sum > max) max = sum;
+		--n;
+	}
+	cout << max;
+	return 0;
+}
