1b12907d0aed8df2512a9891e97e56c33a247f2b
Chaigidel <huaibo2016@gmail.com>
2017-11-10 19:45:23 +0800

    Review heap

--- complete/P3378.cpp
+++ complete/P3378.cpp
@@ -1,33 +1,41 @@
-#include <bits/stdc++.h>
-int heap[100000], tot = 0;
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::cerr;
+int tree[1000001], tot = 0;
 void siftup(){
-    for (int i=tot,j=i>>1;j;i=j, j=i>>1)
-        if (heap[j] > heap[i]) std::swap(heap[j], heap[i]);
+    for (int i=tot, j=i>>1; j>=1; i=j, j=i>>1){
+        if (tree[j] > tree[i]) std::swap(tree[j], tree[i]);
+    }
 }
 void siftdown(){
-    for (int i=1, j=i<<1;j<=tot;i=j, j=i<<1){
-        if ((j+1 <= tot) && (heap[j+1] < heap[j])) j++;
-        if (heap[j] > heap[i]) break;
-        std::swap(heap[j], heap[i]);
+    for (int i=1, j=i<<1; j<=tot; i=j, j=i<<1){
+        if (j+1 <= tot && tree[j] > tree[j+1]) ++j;
+        if (tree[i] > tree[j]) std::swap(tree[i], tree[j]);
+        else break;
     }
 }
 int main(){
-	int i, n, m;
-    std::ios::sync_with_stdio(0);
-	std::cin >> n;
-	for (i=1; i<=n; i++) {
-		std::cin >> m;
-		if (m == 1) {
-			std::cin >> heap[++tot];
-			siftup();
-		}
-		else if (m == 2) {
-			std::cout << heap[1] << std::endl;
-		}
-		else if (m == 3) {
-			heap[1] = heap[tot--];
-			siftdown();
-		}
-	}
-	return 0;
+    std::ios::sync_with_stdio(false);
+    int n, x;
+    cin >> n;
+    for (int i=0; i<n; ++i){
+        cin >> x;
+        if (x == 1){
+            cin >> x;
+            tree[++tot] = x;
+            siftup();
+            continue;
+        }
+        if (x == 2){
+            cout << tree[1] << endl;
+            continue;
+        }
+        if (x == 3){
+            tree[1] = tree[tot--];
+            siftdown();
+        }
+    }
+    return 0;
 }
