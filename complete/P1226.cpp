e76b6620c655ccff98731fda1d4787d96d4a3ab9
Chaigidel <sathariel.conceal@gmail.com>
2018-06-16 09:39:20 +0800

    P1226

---
+++ complete/P1226.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+int main(){
+    long long b, p, k, ans = 1;
+    cin >> b >> p >> k;
+    cout<<b<<"^"<<p<<" mod "<<k<<"=";
+    while (p > 0){
+        if (p % 2 != 0){
+            ans = ans *b %k;
+        }
+        b = b*b%k;
+        p = p>>1;
+    }
+    cout << ans%k;
+    return 0;
+}
