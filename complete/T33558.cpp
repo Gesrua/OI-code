0d9199eaf28b1ba3257bb33b04ef37cb2f02e773
Chaigidel <sathariel.conceal@gmail.com>
2018-06-23 20:32:29 +0800

    3rd contest

---
+++ complete/T33558.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using std::cin; using std::cout; using std::cerr; using std::endl; using std::min; using std::max;
+int m, n, lim, t;
+long long sum[102][102] = {}, dp[102];
+bool flag;
+int main(){
+	std::ios::sync_with_stdio(false);
+	cin >> m >> n;
+	for (int i = 1; i <= m; ++i){
+		for (int j = 1; j <= n; ++j){
+			cin >> t;
+			sum[i][j] = sum[i - 1][j] + t;
+		}
+	}
+	// for (int i = 1; i <= m; ++i){
+	//  for (int j = 1; j <= n; ++j){
+	//      cerr << sum[i][j] << ' ';
+	//  }
+	//  cerr << endl;
+	// }
+	long long ans = -2141111111;
+	for (register int i = 0; i < m; ++i){
+		for (register int j = i + 1; j <= m; ++j){
+			dp[0] = -2100000000;
+			for (register int k = 1; k <= n; ++k){
+				if (dp[k - 1] < 0){
+					// dp[k] = sum[j][k] - sum[i][k];
+					dp[k] = dp[k - 1] + sum[j][k] - sum[i][k];
+					if (dp[k] > ans)
+						ans = dp[k];
+					dp[k] -= dp[k - 1];
+					if (j - i > 1 && dp[k] > ans){
+						ans = dp[k];
+					}
+				}else{
+					dp[k] = dp[k - 1] + sum[j][k] - sum[i][k];
+					if (dp[k] > ans)
+						ans = dp[k];
+				}
+			}
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
