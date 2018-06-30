951d58a0d52bcc6205d64123bd844135ad648c1f
Chaigidel <sathariel.conceal@gmail.com>
2018-06-30 23:27:12 +0800

    效实 Contest 8119

--- complete/P3952.cpp
+++ complete/P3952.cpp
@@ -22,60 +22,60 @@ bool is_legal(int l, int r){
 		if (prog[i][0] == 'F'){
 			++loop;
 			if (flag[prog[i][2]] != 0) return false;
-            ++flag[prog[i][2]];
+			++flag[prog[i][2]];
 			s.push(prog[i][2]);
 		}else{
-            --loop;
-            if (loop < 0) return false;
-            --flag[s.top()];
-            s.pop();
+			--loop;
+			if (loop < 0) return false;
+			--flag[s.top()];
+			s.pop();
 		}
 	}
-    if (loop != 0 || !s.empty()) return false;
-    return true;
+	if (loop != 0 || !s.empty()) return false;
+	return true;
 }
 int calc(int l){
-    if (l > pipei[l]) return 0;
-    int k = 0, ans = 0;
-    if (prog[l].find('n') == std::string::npos){
-        // 起始和结束都是数字，判断是否可以进入循环
-        int le = 0;
-        int i = 4, r = 0;
-    	while (std::isdigit(prog[l][i])){
-            // cerr << prog[l][i] << prog[l][i+1] << endl;
-            // cerr << i+1 << std::isdigit(prog[l][i+1]) << endl;
-    		le *= 10;
-    		le += prog[l][i] - '0';
-    		++i;
-            // cerr << i << std::isdigit(prog[l][i]) << endl;
-    	}
-        ++i;
-        // cerr << i << endl;
-        while (std::isdigit(prog[l][i]) && i < prog[l].size()){
-    		r *= 10;
-    		r += prog[l][i] - '0';
-    		++i;
-    	}
-        // cerr << le << ' ' << r;
-        if (le > r) {return 0;}
-        k = 0;
-    }else{
-        if (prog[l][4] == 'n'){
-            if (prog[l][6] == 'n'){
-                // 两个都是 n 可以进入
-                k = 0;
-            }
-            else return 0; // n > 100 不可以进入
-        }else{
-            k = 1; // 可以进入，复杂度 O(n)
-        }
-    }
-    if (l+1 == pipei[l]) return k;
-    for (int i = l + 1; i < pipei[l]; i = pipei[i]+1){
-        // cerr << i << ' ';
-        ans = max(calc(i), ans);
-    }
-    return k + ans;
+	if (l > pipei[l]) return 0;
+	int k = 0, ans = 0;
+	if (prog[l].find('n') == std::string::npos){
+		// 起始和结束都是数字，判断是否可以进入循环
+		int le = 0;
+		int i = 4, r = 0;
+		while (std::isdigit(prog[l][i])){
+			// cerr << prog[l][i] << prog[l][i+1] << endl;
+			// cerr << i+1 << std::isdigit(prog[l][i+1]) << endl;
+			le *= 10;
+			le += prog[l][i] - '0';
+			++i;
+			// cerr << i << std::isdigit(prog[l][i]) << endl;
+		}
+		++i;
+		// cerr << i << endl;
+		while (std::isdigit(prog[l][i]) && i < prog[l].size()){
+			r *= 10;
+			r += prog[l][i] - '0';
+			++i;
+		}
+		// cerr << le << ' ' << r;
+		if (le > r){return 0;}
+		k = 0;
+	}else{
+		if (prog[l][4] == 'n'){
+			if (prog[l][6] == 'n'){
+				// 两个都是 n 可以进入
+				k = 0;
+			}
+			else return 0; // n > 100 不可以进入
+		}else{
+			k = 1; // 可以进入，复杂度 O(n)
+		}
+	}
+	if (l + 1 == pipei[l]) return k;
+	for (int i = l + 1; i < pipei[l]; i = pipei[i] + 1){
+		// cerr << i << ' ';
+		ans = max(calc(i), ans);
+	}
+	return k + ans;
 }
 int main(){
 	int n, in, k, l, t;
@@ -98,41 +98,41 @@ int main(){
 			}
 		}
 		//
-        cin.get();
+		cin.get();
 		for (int i = 0; i < l; ++i) std::getline(cin, prog[i]);
 
 		if (is_legal(0, l)){
-            // 循环匹配
-            std::stack<int> s;
-            for (int i = 0; i < l; ++i){
-                if (prog[i][0] == 'F') {
-                    s.push(i);
-                }
-                if (prog[i][0] == 'E'){
-                    pipei[s.top()] = i;
-                    pipei[i] = 0;
-                    s.pop();
-                }
-            }
+			// 循环匹配
+			std::stack<int> s;
+			for (int i = 0; i < l; ++i){
+				if (prog[i][0] == 'F'){
+					s.push(i);
+				}
+				if (prog[i][0] == 'E'){
+					pipei[s.top()] = i;
+					pipei[i] = 0;
+					s.pop();
+				}
+			}
 
 
-            k = 0;
+			k = 0;
 
 
-            for (int i = 0; i < l; i = pipei[i]+1){
-                // cerr << i << ' ';
-                k = max(calc(i), k);
-            }
+			for (int i = 0; i < l; i = pipei[i] + 1){
+				// cerr << i << ' ';
+				k = max(calc(i), k);
+			}
 
-            if (k == t){
-                cout << "Yes\n";
-            }else{
-                cout << "No\n";
-            }
-            // cerr << "Right\n";
+			if (k == t){
+				cout << "Yes\n";
+			}else{
+				cout << "No\n";
+			}
+			// cerr << "Right\n";
 		}else{
-            cout << "ERR\n";
-        }
+			cout << "ERR\n";
+		}
 
 	}
 	return 0;
--- complete/T33975.cpp
+++ complete/T33975.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using std::cin; using std::cout; using std::cerr; using std::endl; using std::min; using std::max;
 int main(){
-    int n;
-    long long a[100][2000] = {};
-    cin >> n;
-    a[0][0] = 1;
-    if (n%2 != 0){
-        cout << "nonono.";
-        return 0;
-    }
-    n /= 2;
-    for (int i = 0; i <= n; ++i){
-        for (int j = 0; j <= n; ++j){
-            a[i][j+1] += a[i][j];
-            a[i+1][j] += a[i][j];
-        }
-    }
-    cout << a[n][n];
-    return 0;
+	int n;
+	long long a[100][2000] = {};
+	cin >> n;
+	a[0][0] = 1;
+	if (n % 2 != 0){
+		cout << "nonono.";
+		return 0;
+	}
+	n /= 2;
+	for (int i = 0; i <= n; ++i){
+		for (int j = 0; j <= n; ++j){
+			a[i][j + 1] += a[i][j];
+			a[i + 1][j] += a[i][j];
+		}
+	}
+	cout << a[n][n];
+	return 0;
 }
