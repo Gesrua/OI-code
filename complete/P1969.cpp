e0d519497603da7ea01e8407ac913d362b5990ec
Chaigidel <sathariel.conceal@gmail.com>
2018-07-11 21:53:19 +0800

    NOIP

---
+++ complete/P1969.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cin; using std::cout; using std::cerr; using std::endl;using std::max;
+int main(){
+    int now, n, prev, ans;
+    std::ios::sync_with_stdio(false);    
+    cin >> n >> prev;
+    ans = prev;
+    for (int i = 1; i < n; ++i){
+        cin >> now;
+        ans += max(0, now-prev);
+        prev = now;
+    }
+    cout << ans;
+    return 0;
+}
