cb2fb1fa5ee859d4dddbcf3ea556d0490dc8e17b
Chaigidel <sathariel.conceal@gmail.com>
2018-07-27 08:32:42 +0800

    贪心

---
+++ complete/P2909.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using std::cin; using std::cout; using std::cerr; using std::endl; using std::sort; using std::min;
+int main(){
+	int n, m, d, l, s[50010], h[50010] = {};
+	std::ios::sync_with_stdio(false);
+	cin >> n >> m >> d >> l;
+	for (register int i = 1; i <= n; ++i) cin >> s[i];
+	sort(s + 1, s + 1 + n);
+	int ans = 0;
+	int i = 1, j = 1;
+	while (s[i] < l) ++i;
+	for (; i <= n; ++i){
+		if (s[i] - h[j] * d >= l){
+			++ans,
+			++h[j],
+			j = j % m + 1;
+		}
+	}
+	cout << ans;
+	return 0;
+}
