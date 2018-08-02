8c3b023285f010eaafa9f748f01753b267c0336a
Chaigidel <sathariel.conceal@gmail.com>
2018-08-02 10:55:35 +0800

    STL sort+unique

--- complete/P1059.cpp
+++ complete/P1059.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
+#include <algorithm>
 int main(){
-	int n, sum = 0, t;
-	bool a[1002] = {};
+	int n, sum = 0, a[101] = {};
 	using namespace std;
 	cin >> n;
 	for (int i = 0; i < n; i++){
-		cin >> t;
-		if (a[t] == false){
-			a[t] = true;
-			sum++;
-		}
+		cin >> a[i];
 	}
+    std::sort(a, a+n);
+    sum = std::unique(a, a+n) - a;
 	cout << sum << endl;
-	for (int i = 0; i <= 1000; i++){
-		if (a[i]) cout << i << ' ';
-	}
+    for (int i = 0; i < sum; ++i)
+        cout << a[i] << ' ';
 	return 0;
 }
