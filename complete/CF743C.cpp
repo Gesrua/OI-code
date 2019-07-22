f93de5e3ee25f2063fc9524d5ce116c0672f5378
Chaigidel <chaigidel@outlook.com>
2019-07-22 23:26:40 +0800

    构造

---
+++ complete/CF743C.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#define rep(i, l, r) for (int i = (l); i <= (r); ++i)
+#define per(i, l, r) for (int i = (l); i >= (r); --i)
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::make_pair;
+using std::pair;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef unsigned int ui;
+int main() {
+#ifdef LOCAL
+    freopen("input", "r", stdin);
+#endif
+    std::ios::sync_with_stdio(false);
+    cout.tie(0);
+    ll n;
+    cin >> n;
+    if (n == 1)
+        cout << -1;
+    else
+        cout << n << ' ' << (n + 1) << ' ' << n * (n + 1) << endl;
+    return 0;
+}
