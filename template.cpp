b9e8a4f238b0d048d58efa23ca3d875f387c8d57
Chaigidel <chaigidel@outlook.com>
2019-08-10 09:33:43 +0800

    更新模板

--- template.cpp
+++ template.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <bitset>
 #include <cassert>
 #include <cmath>
 #include <cstdio>
@@ -7,28 +8,102 @@
 #include <iostream>
 #include <map>
 #include <queue>
+#include <random>
 #include <set>
 #include <stack>
 #include <string>
 #include <utility>
 #include <vector>
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("inline")
+// #pragma GCC optimize("-fgcse")
+// #pragma GCC optimize("-fgcse-lm")
+// #pragma GCC optimize("-fipa-sra")
+// #pragma GCC optimize("-ftree-pre")
+// #pragma GCC optimize("-ftree-vrp")
+// #pragma GCC optimize("-fpeephole2")
+// #pragma GCC optimize("-ffast-math")
+// #pragma GCC optimize("-fsched-spec")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC optimize("-falign-jumps")
+// #pragma GCC optimize("-falign-loops")
+// #pragma GCC optimize("-falign-labels")
+// #pragma GCC optimize("-fdevirtualize")
+// #pragma GCC optimize("-fcaller-saves")
+// #pragma GCC optimize("-fcrossjumping")
+// #pragma GCC optimize("-fthread-jumps")
+// #pragma GCC optimize("-funroll-loops")
+// #pragma GCC optimize("-fwhole-program")
+// #pragma GCC optimize("-freorder-blocks")
+// #pragma GCC optimize("-fschedule-insns")
+// #pragma GCC optimize("inline-functions")
+// #pragma GCC optimize("-ftree-tail-merge")
+// #pragma GCC optimize("-fschedule-insns2")
+// #pragma GCC optimize("-fstrict-aliasing")
+// #pragma GCC optimize("-fstrict-overflow")
+// #pragma GCC optimize("-falign-functions")
+// #pragma GCC optimize("-fcse-skip-blocks")
+// #pragma GCC optimize("-fcse-follow-jumps")
+// #pragma GCC optimize("-fsched-interblock")
+// #pragma GCC optimize("-fpartial-inlining")
+// #pragma GCC optimize("no-stack-protector")
+// #pragma GCC optimize("-freorder-functions")
+// #pragma GCC optimize("-findirect-inlining")
+// #pragma GCC optimize("-fhoist-adjacent-loads")
+// #pragma GCC optimize("-frerun-cse-after-loop")
+// #pragma GCC optimize("inline-small-functions")
+// #pragma GCC optimize("-finline-small-functions")
+// #pragma GCC optimize("-ftree-switch-conversion")
+// #pragma GCC optimize("-foptimize-sibling-calls")
+// #pragma GCC optimize("-fexpensive-optimizations")
+// #pragma GCC optimize("-funsafe-loop-optimizations")
+// #pragma GCC optimize("inline-functions-called-once")
+// #pragma GCC optimize("-fdelete-null-pointer-checks")
 #define rep(i, l, r) for (int i = (l); i <= (r); ++i)
 #define per(i, l, r) for (int i = (l); i >= (r); --i)
 using std::cerr;
-using std::cin;
-using std::cout;
-using std::endl;
 using std::make_pair;
 using std::pair;
 typedef pair<int, int> pii;
 typedef long long ll;
 typedef unsigned int ui;
+
+const int BUF = 50000000;
+struct IOStream {
+    char ibuf[BUF], *s;
+    char obuf[BUF], *oh;
+    IOStream() : s(ibuf), oh(obuf) { ibuf[fread(ibuf, 1, BUF, stdin)] = '\0'; }
+    ~IOStream() { fwrite(obuf, 1, oh - obuf, stdout); }
+    template <typename T>
+    inline IOStream &operator>>(T &x) {
+        while (!isdigit(*s)) s++;
+        for (x = 0; isdigit(*s); s++) x = x * 10 + (*s ^ '0');
+        return *this;
+    }
+    template <typename T>
+    inline IOStream &operator<<(T x) {
+        static char buf[13];
+        register char *top = buf;
+        if (x != 0) {
+            for (register int t; x;) {
+                t = x / 10;
+                *top++ = x - t * 10 + 48;
+                x = t;
+            }
+            while (top != buf) *oh++ = *--top;
+        } else {
+            *oh++ = '0';
+        }
+        *oh++ = ' ';
+        return *this;
+    }
+} cin;
+IOStream &cout = cin;
+
 int main() {
 #ifdef LOCAL
     freopen("input", "r", stdin);
 #endif
-    std::ios::sync_with_stdio(false);
-    cout.tie(0);
 
     return 0;
 }
