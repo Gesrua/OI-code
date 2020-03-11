659d4e24ad5213f6840eebb9ef23ca9304bf906f
Chaigidel <chaigidel@outlook.com>
2020-03-11 22:20:37 +0800

    template

---
+++ template-simple.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, l, r) for (int i = (l); i <= (r); ++i)
+#define per(i, l, r) for (int i = (l); i >= (r); --i)
+using std::cerr; using std::cin; using std::cout; using std::endl; using std::make_pair; using std::pair;
+typedef long long ll; typedef unsigned int ui; typedef unsigned long long ull; typedef pair<int, int> pii; 
+int main() {
+    std::ios::sync_with_stdio(false);
+    cout.tie(0);
+    
+    return 0;
+}
